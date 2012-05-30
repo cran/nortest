536733e539857d96feeb1f276a3c16d925cf083a
Uwe Ligges <ligges@statistik.tu-dortmund.de>
2012-05-30 00:00:00 +0000

    version 1.0-2

--- R/cvm.test.R
+++ R/cvm.test.R
@@ -7,7 +7,10 @@ function (x)
     if (n < 8) 
         stop("sample size must be greater than 7")
     p <- pnorm((x - mean(x))/sd(x))
-    W <- (1/(12 * n) + sum((p - (2 * seq(1:n) - 1)/(2 * n))^2))
+    W <- (1/(12 * n) + 
+        sum(
+            (p - (2 * seq(1:n) - 1)/(2 * n))^2
+        ))
     WW <- (1 + 0.5/n) * W
     if (WW < 0.0275) {
         pval <- 1 - exp(-13.953 + 775.5 * WW - 12542.61 * WW^2)
@@ -18,8 +21,12 @@ function (x)
     else if (WW < 0.092) {
         pval <- exp(0.886 - 31.62 * WW + 10.897 * WW^2)
     }
-    else {
+    else if (WW < 1.1) {
         pval <- exp(1.111 - 34.242 * WW + 12.832 * WW^2)
+    }
+    else {
+        warning("p-value is smaller than 7.37e-10, cannot be computed more accurately")
+        pval <- 7.37e-10
     }
     RVAL <- list(statistic = c(W = W), p.value = pval, method = "Cramer-von Mises normality test", 
         data.name = DNAME)
